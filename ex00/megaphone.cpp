d0093aa42846e4d5308c2bd8b65c9ad60f0a2743
MJ 42 <mingjuntan93@gmail.com>
2025-06-07 17:17:56 +0800

    Finished ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,14 +6,39 @@
 /*   By: mintan <mintan@stuident.42singapore.sg>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 15:31:42 by mintan            #+#    #+#             */
-/*   Updated: 2025/06/07 15:33:25 by mintan           ###   ########.fr       */
+/*   Updated: 2025/06/07 17:16:37 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
-int	main(void)
+
+#define DEFAULT_MSG "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n"
+
+/* Description: Takes in a string and prints it out in uppercase.
+   static_cast is used for robustness
+*/
+void	print_upper(char *str)
 {
-	std::cout <<"test\n";
+	char	c;
+
+	for(int i = 0; str[i] != '\0'; i++)
+	{
+		c = std::toupper(static_cast<unsigned char>(str[i]));
+		std::cout << c;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+			print_upper(argv[i]);
+		std::cout << "\n";
+	}
+	else
+		std::cout << DEFAULT_MSG;
 	return (0);
 }
