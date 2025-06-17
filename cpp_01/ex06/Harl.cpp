ee3540e30d9e9b8fcc60068bc35481e9188d5b9e
MJ <mingjuntan93@gmail.com>
2025-06-18 03:36:24 +0800

    Completed ex06

---
+++ cpp_01/ex06/Harl.cpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 10:27:17 by mintan            #+#    #+#             */
+/*   Updated: 2025/06/18 03:34:20 by mintan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl(int argc, char *argv[])
+{
+	this->setLevel(argc, argv);
+	this->complain();
+	return;
+}
+
+Harl::~Harl(void)
+{
+	return;
+}
+
+void	Harl::complain(void) const
+{
+	switch (this->_level)
+	{
+		case 0:
+			this->debug();
+			// fall through
+		case 1:
+			this->info();
+			// fall through
+		case 2:
+			this->warning();
+			// fall through
+		case 3:
+			this->error();
+			// fall through
+		default:
+			std::cout << MSG_DEF <<  std::endl;
+			break;
+	}
+}
+
+void	Harl::debug(void) const
+{
+	std::cout << MSG_DEBUG <<  std::endl;
+}
+
+void	Harl::info(void) const
+{
+	std::cout << MSG_INFO <<  std::endl;
+}
+
+void	Harl::warning(void) const
+{
+	std::cout << MSG_WARN <<  std::endl;
+}
+
+void	Harl::error(void) const
+{
+	std::cout << MSG_ERR <<  std::endl;
+}
+
+void	Harl::setLevel(int argc, char *argv[])
+{
+	std::string	lv;
+
+	if (argc == 1 || argc > 2)
+		this->_level = 4;
+	else
+	{
+		lv = argv[1];
+		if (lv == "DEBUG")
+			this->_level = 0;
+		else if (lv == "INFO")
+			this->_level = 1;
+		else if (lv == "WARNING")
+			this->_level = 2;
+		else if (lv == "ERROR")
+			this->_level = 3;
+		else
+			this->_level = 4;
+	}
+}
+
--- cpp_01/ex06/Harl.hpp
+++ cpp_01/ex06/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 10:22:57 by mintan            #+#    #+#             */
-/*   Updated: 2025/06/18 02:54:10 by mintan           ###   ########.fr       */
+/*   Updated: 2025/06/18 03:12:09 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,33 @@
 #include <sstream>
 
 #define	MSG_DEBUG "[ DEBUG ]\nI love having extra bacon for my 7XL-double- \
-cheese-triple pickle-specialketchup burger. I really do!"
+cheese-triple pickle-specialketchup burger. I really do!\n"
 #define	MSG_INFO "[ INFO ]\nI cannot believe adding extra bacon costs more \
 money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be \
-asking for more!"
+asking for more!\n"
 #define	MSG_WARN "[ WARNING ]\nI think I deserve to have some extra bacon for \
 free. I’ve been coming for years, whereas you started working here just last \
-month."
+month.\n"
 #define	MSG_ERR "[ ERROR ]\nThis is unacceptable! I want to speak to the manager \
-now."
+now0.\n"
+#define	MSG_DEF "[ DEFAULT ]\nWhat's worse than a karen is a karen who can't \
+spell.\n"
 
 class	Harl
 {
 	public:
-		Harl(void);
+		Harl(int argc, char *argv[]);
 		~Harl(void);
-		void	complain(std::string level);
+		void	complain(void) const;
 
 	private:
-		void	debug(void);
-		void	info(void);
-		void	warning(void);
-		void	error(void);
+		void	debug(void) const;
+		void	info(void) const;
+		void	warning(void) const;
+		void	error(void) const;
+		void	setLevel(int argc, char *argv[]);
+		int		_level;
+
 };
 
 #endif
