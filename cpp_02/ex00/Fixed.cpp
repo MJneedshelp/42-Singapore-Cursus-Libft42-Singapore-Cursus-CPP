12254b538980447c5883dd84f3181b901f446ece
MJ <mingjuntan93@gmail.com>
2025-06-24 03:18:04 +0800

    completed ex00

--- cpp_02/ex00/Fixed.cpp
+++ cpp_02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 00:48:23 by mintan            #+#    #+#             */
-/*   Updated: 2025/06/23 01:56:17 by mintan           ###   ########.fr       */
+/*   Updated: 2025/06/24 02:49:16 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	const Fixed::_numFractBits = 8;
 
-Fixed::Fixed(void): _value(0)
+Fixed::Fixed(void): _raw(0)
 {
 	std::cout << "Default constructor called" << std::endl;
 	return;
@@ -37,6 +37,17 @@ Fixed&	Fixed::operator=(Fixed const &src)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &src)
-		//code to assign the value of src to this
+		this->_raw = src.getRawBits();
 	return (*this);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_raw = raw;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_raw);
 }
--- cpp_02/ex00/Fixed.hpp
+++ cpp_02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 00:36:44 by mintan            #+#    #+#             */
-/*   Updated: 2025/06/23 01:41:42 by mintan           ###   ########.fr       */
+/*   Updated: 2025/06/24 02:38:54 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,12 @@ class Fixed
 		Fixed(Fixed const &src);
 		~Fixed(void);
 		Fixed&	operator=(Fixed const &src);
-		void	setRawBits(int const raw) const;
-		int		getRawBits(void);
-
+		void	setRawBits(int const raw);
+		int		getRawBits(void) const;
 
 	private:
-		int		_value;
+		int					_raw;
 		static const int	_numFractBits;
-
 };
 
 
