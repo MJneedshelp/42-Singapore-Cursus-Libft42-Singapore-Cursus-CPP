0922becbd0d16964d0d091519c9a2f6256b5c9f4
MJ <mingjuntan93@gmail.com>
2025-07-06 09:19:04 +0800

    Updated testing for ex00

--- cpp_04/ex00/main.cpp
+++ cpp_04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 14:55:02 by mintan            #+#    #+#             */
-/*   Updated: 2025/07/06 08:49:30 by mintan           ###   ########.fr       */
+/*   Updated: 2025/07/06 09:17:17 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,34 +18,35 @@
 
 int	main(void)
 {
+	std::cout << "\n========Test: OCF========" << std::endl;
+	Animal	animalA;
+	Animal	animalF(animalA);
+	Cat		animalB;
+	Cat		animalC(animalB);
+	Dog		animalD;
+	Dog		animalE(animalD);
+
+	std::cout << "Memory address of Animal A: " << &animalA << std::endl;
+	std::cout << "Memory address of Animal F: " << &animalF << std::endl;
+	std::cout << "Memory address of Animal B: " << &animalB << std::endl;
+	std::cout << "Memory address of Animal C: " << &animalC << std::endl;
+	std::cout << "Memory address of Animal D: " << &animalD << std::endl;
+	std::cout << "Memory address of Animal E: " << &animalE << std::endl;
+
 	std::cout << "\n========Test: Animals========" << std::endl;
 	const Animal	*meta = new Animal(DEF_ANIMAL_TYPE);
 	const Animal	*i = new Cat();
 	const Animal	*j = new Dog();
-	const Animal	*l = new Animal(*i);
-	const Animal	*m = new Animal(*j);
-
 
 	std::cout << i->getType() << " " << std::endl;
 	std::cout << j->getType() << " " << std::endl;
-	std::cout << l->getType() << " " << std::endl;
-	std::cout << m->getType() << " " << std::endl;
-
-	std::cout << "Memory address of i: " << &i << std::endl;
-	std::cout << "Memory address of l: " << &l << std::endl;
-	std::cout << "Memory address of j: " << &j << std::endl;
-	std::cout << "Memory address of m: " << &m << std::endl;
 
 	i->makeSound();
 	j->makeSound();
-	l->makeSound();
-	m->makeSound();
-
 	delete (meta);
+
 	delete (i);
 	delete (j);
-	delete (l);
-	delete (m);
 
 	std::cout << "\n========Test: Wrong Animals========" << std::endl;
 	const	WrongAnimal	*atem = new WrongAnimal();
@@ -59,5 +60,6 @@ int	main(void)
 	delete (atem);
 	delete (k);
 
+	std::cout << "\n========Test: Destructors========" << std::endl;
 	return (0);
 }
