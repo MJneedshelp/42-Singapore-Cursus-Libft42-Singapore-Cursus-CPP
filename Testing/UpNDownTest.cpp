8538cfaabf087a993c4f79a4b9bafe2bcb5c4df7
MJ <mingjuntan93@gmail.com>
2025-07-18 21:28:36 +0800

    Test upcast and downcast

--- Testing/UpNDownTest.cpp
+++ Testing/UpNDownTest.cpp
@@ -6,7 +6,7 @@
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 20:30:10 by mintan            #+#    #+#             */
-/*   Updated: 2025/07/18 21:01:55 by mintan           ###   ########.fr       */
+/*   Updated: 2025/07/18 21:28:03 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,9 @@ int	main(void)
 	Parent	*b = &a;			//Implicit "reinterpretation" cast
 	Parent	*c = (Parent *)&a;	//Explicit "reinterpretation" cast
 
+	Parent	*d = &a;			//Implicit Upcast from specific to -> generic | OK
+	Child1	*e = d;				//Implicit Downcast from generic -> specific | NOK
+	Child2	*f = (Child2 *)d;	//Explicit Downcast from generic -> specific | OK | But what's the point?
 
 
 
@@ -32,7 +35,9 @@ int	main(void)
 	std::cout << "Memory address of a: " << &a << std::endl;
 	std::cout << "Memory address of *b: " << b << std::endl;
 	std::cout << "Memory address of *c: " << c << std::endl;
+	std::cout << "Memory address of *d: " << d << std::endl;
+	std::cout << "Memory address of *f: " << f << std::endl;
 
-
+	return (0);
 }
 
