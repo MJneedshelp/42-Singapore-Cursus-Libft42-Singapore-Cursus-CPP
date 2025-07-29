ef1b526e667d537449378d7ffd0c18288bd05d73
MJ 42 <mingjuntan93@gmail.com>
2025-07-29 08:59:37 +0800

    added functions to determine type

--- cpp_06/ex00_2/ScalarConverter.cpp
+++ cpp_06/ex00_2/ScalarConverter.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
+/*   By: mintan <mintan@stuident.42singapore.sg>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 22:40:54 by mintan            #+#    #+#             */
-/*   Updated: 2025/07/29 08:21:15 by mintan           ###   ########.fr       */
+/*   Updated: 2025/07/29 08:57:06 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,104 @@ bool	ScalarConverter::_isInt(const std::string &input)
 	return (false);
 }
 
+/* Helper functions - Determine Double */
+
+bool	ScalarConverter::_isDoublePLiteral(const std::string &input)
+{
+	std::string	inputUpper;
+
+	inputUpper = _strToUpper(input);
+	if (inputUpper == "NAN" || inputUpper == "INF" || \
+	inputUpper == "+INF" || inputUpper == "-INF")
+		return (true);
+	return (false);
+}
+
+bool	ScalarConverter::_isDouble(const std::string &input)
+{
+	int		numDots;
+	char	c;
+
+	if (_isDoublePLiteral(input))
+		return (true);
+	numDots = 0;
+	for (unsigned int i = 0; i < input.length(); ++i)
+	{
+		c = static_cast<char>(input[i]);
+		if (i == 0 && c == '-')
+			continue;
+		if (!std::isdigit(c))
+		{
+			if (c == '.')
+				++numDots;
+			else
+				return (false);
+		}
+	}
+	if (numDots > 1)
+		return (false);
+	return(true);
+}
+
+/* Helper functions - Determine Float */
+
+bool	ScalarConverter::_isFloatPLiteral(const std::string &input)
+{
+	std::string	inputUpper;
+
+	inputUpper = _strToUpper(input);
+	if (inputUpper == "NANF" || inputUpper == "INFF" || \
+	inputUpper == "+INFF" || inputUpper == "-INFF")
+		return (true);
+	return (false);
+}
+
+bool	ScalarConverter::_isFloat(const std::string &input)
+{
+	int		numDots;
+	char	c;
+
+	if (_isFloatPLiteral(input))
+		return (true);
+	numDots = 0;
+	for (unsigned int i = 0; i < input.length(); ++i)
+	{
+		c = static_cast<char>(input[i]);
+		if (i == 0 && c == '-')
+			continue;
+		if (!std::isdigit(c))
+		{
+			if (c == '.')
+				++numDots;
+			else if ((c == 'f' || c == 'F') && i == input.length() - 1)
+				continue;
+			else
+				return (false);
+		}
+	}
+	if (numDots > 1)
+		return (false);
+	return(true);
+}
+
+/* Helper functions - General */
+
+std::string	ScalarConverter::_strToUpper(const std::string &input)
+{
+	std::string	strUpper;
+
+	strUpper = input;
+	for (unsigned int i = 0; i < input.length(); ++i)
+		strUpper[i] = std::toupper(static_cast<unsigned char>(input[i]));
+	return (strUpper);
+}
+
+
+
+
+
+
+
 
 
 
@@ -63,7 +161,11 @@ int	ScalarConverter::_determineType(const std::string &input)
 		return (CHAR);
 	if (_isInt(input))
 		return (INT);
-	return (-1);
+	if (_isDouble(input))
+		return (DOUBLE);
+	if (_isFloat(input))
+		return (FLOAT);
+	return (NOTINLIST);
 }
 
 
--- cpp_06/ex00_2/ScalarConverter.hpp
+++ cpp_06/ex00_2/ScalarConverter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
+/*   By: mintan <mintan@stuident.42singapore.sg>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 22:40:42 by mintan            #+#    #+#             */
-/*   Updated: 2025/07/29 08:17:05 by mintan           ###   ########.fr       */
+/*   Updated: 2025/07/29 08:54:07 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,18 @@ class ScalarConverter
 		static	bool	_isWholeNum(const std::string &input);
 		static	bool	_withinIntLimits(const std::string &input);
 		static	bool	_isInt(const std::string &input);
+
+		/* Helper functions - Determine Double */
+		static	bool	_isDoublePLiteral(const std::string &input);
+		static	bool	_isDouble(const std::string &input);
+
+		/* Helper functions - Determine Float */
+		static	bool	_isFloatPLiteral(const std::string &input);
+		static	bool	_isFloat(const std::string &input);
+
+		/* Helper functions - General */
+		static	std::string	_strToUpper(const std::string &input);
+
 
 
 
