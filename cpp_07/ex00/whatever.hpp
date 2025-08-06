cdf316fbd66ac7802541bee111fc939877387ed4
MJ <mingjuntan93@gmail.com>
2025-08-06 23:44:05 +0800

    Updated whatever.hpp

--- cpp_07/ex00/whatever.hpp
+++ cpp_07/ex00/whatever.hpp
@@ -6,9 +6,12 @@
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 13:19:46 by mintan            #+#    #+#             */
-/*   Updated: 2025/08/06 23:32:28 by mintan           ###   ########.fr       */
+/*   Updated: 2025/08/06 23:43:18 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef WHATEVER_H
+# define WHATEVER_H
 
 #include <iostream>
 
@@ -34,3 +37,4 @@ T const&	max(const T &x, const T &y)
 	return (x >= y ? x : y);
 }
 
+#endif
