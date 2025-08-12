cef5a0c0cf45240e0f1953add2087714481ab313
MJ 42 <mingjuntan93@gmail.com>
2025-08-12 10:58:06 +0800

    Tried for_each

--- cpp_08/tests/stltest.cpp
+++ cpp_08/tests/stltest.cpp
@@ -6,12 +6,20 @@
 /*   By: mintan <mintan@stuident.42singapore.sg>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 10:10:50 by mintan            #+#    #+#             */
-/*   Updated: 2025/08/12 10:45:39 by mintan           ###   ########.fr       */
+/*   Updated: 2025/08/12 10:57:51 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <list>
+#include <algorithm>
+
+
+void	prInt(int i)
+{
+	std::cout << i << std::endl;
+}
+
 
 int	main(void)
 {
@@ -26,16 +34,12 @@ int	main(void)
 		listInt.push_back(99);
 		listInt.push_back(155);
 
+		std::cout << "============= Using const iterator =============" << std::endl;
 		for (itInt = listInt.begin(); itInt != itIntEnd; ++itInt)
 			std::cout << *itInt << std::endl;
+
+		std::cout << "\n============= Using for_each =============" << std::endl;
+		for_each(listInt.begin(), listInt.end(), prInt);
 	}
-
-
-
-
 	return (0);
-
-
-
-
 }
