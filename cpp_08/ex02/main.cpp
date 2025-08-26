69dd013b3fd2da6be03e890e4d044149387af5e3
MJ <mingjuntan93@gmail.com>
2025-08-27 00:30:36 +0800

    added typename test

--- cpp_08/ex02/main.cpp
+++ cpp_08/ex02/main.cpp
@@ -3,10 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mintan <mintan@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 14:04:13 by mintan            #+#    #+#             */
-/*   Updated: 2025/08/26 14:04:14 by mintan           ###   ########.fr       */
+/*   Updated: 2025/08/26 23:51:10 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+
+int	main(void)
+{
+	std::cout << "============= 0. OCF Tests =============" << std::endl;
+	MutantStack<int>	wolverine;
+
+	wolverine.push(24);
+	std::cout << "[Wolverine] Top: " << wolverine.top() << " | Size: " << wolverine.size() << std::endl;
+
+	std::cout << "========== a. Copy Constructor ==========" << std::endl;
+	MutantStack<int>	logan(wolverine);
+
+	std::cout << "[Logan] Top: " << logan.top() << " | Size: " << logan.size() << std::endl;
+
+	std::cout << "========== b. Copy Assignment Operator ==========" << std::endl;
+	MutantStack<int>	weaponX;
+
+	weaponX =  logan;
+	std::cout << "[weaponX] Top: " << weaponX.top() << " | Size: " << weaponX.size() << std::endl;
+
+	std::cout << "========== c. Deep Copy  ==========" << std::endl;
+	wolverine.push(42);
+	std::cout << "[Wolverine] Top: " << wolverine.top() << " | Size: " << wolverine.size() << std::endl;
+	std::cout << "[Logan] Top: " << logan.top() << " | Size: " << logan.size() << std::endl;
+	std::cout << "[weaponX] Top: " << weaponX.top() << " | Size: " << weaponX.size() << std::endl;
+
+
+
+
+
+
+
+
+
+
+
+
+	return (0);
+}
--- cpp_08/tests/typenametest.cpp
+++ cpp_08/tests/typenametest.cpp
@@ -1,36 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*   typenametest.cpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/26 18:48:18 by mintan            #+#    #+#             */
-/*   Updated: 2025/08/27 00:03:08 by mintan           ###   ########.fr       */
+/*   Created: 2025/08/27 00:23:56 by mintan            #+#    #+#             */
+/*   Updated: 2025/08/27 00:30:05 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MUTANTSTACK_H
-# define MUTANTSTACK_H
-
-#include <stack>
-#include <deque>
 #include <iostream>
 
-template<typename T>
-class MutantStack: public std::stack<T>
+class Wrapper
 {
 	public:
-		typedef	typename std::stack<T>::container_type::iterator		iterator;
-		typedef	typename std::stack<T>::container_type::const_iterator	const_iterator;
-
-
-
-	private:
-
-
-
+		typedef	int	inner_type;
 };
-#include "MutantStack.tpp"
-#endif
+
+template<typename T>
+void	test_typedef()
+{
+	typename T::inner_type	x = 42;
+	std::cout<< "Value: " << x << std::endl;
+}
+
+int	main(void)
+{
+	test_typedef<Wrapper>();
+	return (0);
+}
 
